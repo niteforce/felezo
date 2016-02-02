48f97880b09fc41c0d0611a035435dffb2627aad
Dániel Horváth <windows.hd@live.com>
2016-02-02 21:27:44 +0100

    init

---
+++ felezo/felezo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+
+namespace felezo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            WriteLine("X hatvanya?:  ");
+            var fokszam = int.Parse(ReadLine());
+            Write("X^" + fokszam + " = ");
+            var valtozo = float.Parse(ReadLine());
+
+
+            WriteLine("Adj meg egy felso becslest!");
+            var felso = float.Parse(ReadLine());
+            WriteLine("Adj meg egy also becslest!");
+            var also = float.Parse(ReadLine());
+
+
+            float gyok;
+
+            while (Math.Abs(also - felso) > 0.000001)
+            {
+                gyok = (also + felso) /2;
+
+                if (Math.Pow(gyok, fokszam) - valtozo > 0)
+                {
+                    felso = gyok;
+                }
+                else
+                {
+                    also = gyok;
+                }
+            }
+
+            WriteLine("X = " + (also + felso) /2);
+            ReadLine();
+        }
+    }
+}
